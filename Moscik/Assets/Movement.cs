0231a6f60ca047d67c65bb0860bcd700bea74606
Filip Kobiec <fipis14@gmail.com>
2020-01-11 19:48:08 +0100

    ProjStart

---
+++ Moscik/Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")) {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey("s")) {
+            transform.position += transform.TransformDirection(Vector3.back) * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey("a")) {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey("d")) {
+            transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * movementSpeed;
+        }
+    }
+}
---
+++ Moscik/Assets/playerShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShoot : MonoBehaviour {
+    [SerializeField] private ballControll ballPrefab;
+
+    public Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Shoot() {
+       var ballInstance = Instantiate(ballPrefab);
+       ballInstance.transform.position = spawnPoint.position;
+       ballInstance.AddForce(spawnPoint.forward, 20f);
+    }
+}
